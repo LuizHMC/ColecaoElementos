31e18ffc4038d652fa8c9a4119f5bec0cf20ee47
Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>
2019-11-19 13:28:53 -0300

    menu
    Signed-off-by: Luiz Henrique Monteiro de Carvalho <lhmcarvx5@gmail.com>

--- src/colecaoelementos/colecaoElemento.java
+++ src/colecaoelementos/colecaoElemento.java
@@ -21,11 +21,66 @@ public class colecaoElemento {
         Ponto list[] = {p1};
         l1 = new listaPonto(1);
 
+        int escolha;
+        Scanner input    = new Scanner(System.in);
+
         System.out.print(p1+"\n");
         System.out.print(l1.getPontos()+"\n");
         System.out.print(list+"\n");
 
+        System.out.print("--------------------------------------------\n");
+        System.out.print("|");
+        System.out.print("                                          |\n");
+        System.out.print("|        Coleção de elementos         |\n");
+        System.out.print("|");
+        System.out.print("                                          |\n");
+        System.out.print("--------------------------------------------\n");
+        System.out.print("\n");
+        System.out.print("Bem-vindo\n");
 
+        while (true) {
+            System.out.println("----------------------\n");
+            System.out.println("Menu - Calculadora\n");
+            System.out.print("1.) Adicionar um elemento no final da coleção.\n");
+            System.out.print("2.) Adicionar um elemento no final da coleção.\n");
+            System.out.print("3.) Retornar o índice da primeira ocorrência de um elemento especificado na coleção\n");
+            System.out.print("4.) Remover um elemento em uma posição na coleção\n");
+            System.out.print("5.) Calcular a distância dos dois pontos mais distantes na coleção.\n");
+            System.out.print("6.) Retornar uma coleção de pontos contido em uma circunferência.\n");
+            System.out.print("0.) Sair\n");
+            System.out.print("\nDigite sua opção: ");
+            System.out.println("----------------------\n");
+
+            escolha = input.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.println("Adicionar um elemento no final da coleção. ");
+                    break;
+                case 2:
+                    System.out.println("Adicionar um elemento no final da coleção. ");
+                    break;
+                case 3:
+                    System.out.println("Retornar o índice da primeira ocorrência de um elemento especificado na coleção.");
+                    break;
+
+                case 4:
+                    System.out.println("Remover um elemento em uma posição na coleção");
+                    break;
+                case 5:
+                    System.out.println("Calcular a distância dos dois pontos mais distantes na coleção.");
+
+                case 6:
+                    System.out.println("Retornar uma coleção de pontos contido em uma circunferência.");
+
+                case 0:
+                    System.out.println("Ate mais...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcao invalida! Tente novamente.");
+                    break;
+            }
+        }
     }
-
 }
